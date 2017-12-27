bd7568a4f29484bb5257ca5d1b810e67b3a96e01
yoyojacky <yoyojacky@users.noreply.github.com>
2017-12-27 15:00:16 +0800

    fixed a bug
    fixed a bug

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo -e "\033mInstall package's for script.\0m" 
+echo -e "\e[32;40mInstall package's for script. \e[0m"
 sudo apt-get -y update 
 sudo apt-get -y install wiringPi git-core chkconfig
-echo -e "\033m Download RPB driver...\0m"
+echo -e "\e[32;40m Download RPB driver...\e[0m"
 cd ~
 git clone https://github.com/geeekpi/RPB.git
 cd RPB/
@@ -13,9 +13,8 @@ sudo chkconfig --level 2345 remoteswitch on
 echo -e "\e[32:40m Reboot your raspberry and try to press the power button, reboot button and test it.\e[0m"
 for i in 5 4 3 2 1
 do 
-  echo -e "\e[32:40m $i\e[0m"
+  echo -e "\e[32;40m $i\e[0m"
   sleep 1
 done 
 sudo sync
 sudo shutdown -r now
-
