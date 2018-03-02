4bcd15985973e0c4933e43ece57aafaed22c2e62
yoyojacky <yoyojacky@users.noreply.github.com>
2018-03-02 14:54:52 +0800

    update fixed a bug
    update fixed a bug.

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ sudo cp -Rvf remoteswitch /etc/init.d/
 sudo chmod +x /etc/init.d/remoteswitch
 sudo chkconfig --add remoteswitch 2&>/dev/null
 sudo chkconfig --level 2345 remoteswitch on 2&>/dev/null
-sudo sed '/exit/d' /etc/rc.local
-sudo sed '$i gpio mode 25 OUT\ngpio mode 25 1\nexit 0'  /etc/rc.local
+sudo sed -i '/exit/d' /etc/rc.local
+sudo sed -i '$i gpio mode 25 OUT\ngpio mode 25 1\nexit 0'  /etc/rc.local
 echo -e "Change /boot/config.txt configuration"
 echo -e "\e[32:40m Reboot your raspberry and try to press the power button, reboot button and test it.\e[0m"
 for i in 5 4 3 2 1
