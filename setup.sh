32599dfc90112c64c008d3482e1d79b9451fcfeb
Tater Li <admin@lijingquan.net>
2018-04-20 13:30:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo -e "\e[32;40mInstall package's for script. \e[0m"
 sudo apt-get install -y -qq wiringpi
 sudo sh -c " curl -sSL https://raw.githubusercontent.com/geeekpi/RPB-HAT/master/remoteswitch > /bin/remoteswitch "
 sudo chmod a+x /bin/remoteswitch
-sudo sed -i '$i sudo remoteswitch -r 17 -s 18 -n 4 >> /tmp/piboxy.log &' /etc/rc.local
+sudo sed -i '$i sudo remoteswitch &' /etc/rc.local
 echo -e '"\e[32:40m Reboot your raspberry and try to press the power button, reboot button and test it.\e[0m"'
 for i in 5 4 3 2 1
 do 
